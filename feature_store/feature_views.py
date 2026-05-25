80e66fe23f05c7969c75cf758c495fc713c7d1e0
ptrnghieu <ptrnghieu@gmail.com>
2026-05-25 21:18:15 +0700

    [infra] update docker-compose: add kafka, fix networks, feast config

--- feature_store/feature_views.py
+++ feature_store/feature_views.py
@@ -1,8 +1,6 @@
-from feast import FeatureView, Field, FileSource
+from feast import FeatureView, Field
 from feast.types import Int64, Float32, String
 from feast.infra.offline_stores.bigquery_source import BigQuerySource
-
-
 from entities import msno
 from datetime import timedelta
 from dotenv import load_dotenv
@@ -12,16 +10,11 @@ load_dotenv()
 
 GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 BQ_DATASET = os.getenv("BQ_DATASET")
-TABLE_NAME = os.getenv("TABLE_NAME", "features")
+TABLE_NAME = os.getenv("TABLE_NAME", "features_train")
 
-# feature_source = BigQuerySource(
-#     table=f"{GCP_PROJECT_ID}.{BQ_DATASET}.{TABLE_NAME}",
-#     timestamp_field="event_timestamp"        # table bigquery phai co, fix sau
-# )
-
-feature_source = FileSource(
-    path="../data/features.parquet",
-    timestamp_field="event_timestamp",
+feature_source = BigQuerySource(
+    table=f"{GCP_PROJECT_ID}.{BQ_DATASET}.{TABLE_NAME}",
+    timestamp_field="event_timestamp"
 )
 
 kkbox_fv = FeatureView(
@@ -29,20 +22,15 @@ kkbox_fv = FeatureView(
     entities=[msno],
     ttl=timedelta(days=30),
     schema=[
-        # member
         Field(name="city", dtype=Int64),
         Field(name="bd", dtype=Int64),
         Field(name="gender", dtype=String),
         Field(name="registered_via", dtype=Int64),
-
-        #transaction
         Field(name="total_transactions", dtype=Int64),
         Field(name="total_amount_paid", dtype=Float32),
         Field(name="avg_amount_paid", dtype=Float32),
         Field(name="auto_renew_count", dtype=Int64),
         Field(name="cancel_count", dtype=Int64),
-
-        #log
         Field(name="total_log_days", dtype=Int64),
         Field(name="total_secs", dtype=Float32),
         Field(name="avg_daily_secs", dtype=Float32),
